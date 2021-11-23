4fafa9f310c00935103bcffdef6d7483c7ffeedb
Howlhoff <diego.beltran@sansano.usm.cl>
2021-11-23 19:12:18 -0300

    Tarea 3 v0.2

--- Tarea 3/src/App.java
+++ Tarea 3/src/App.java
@@ -1,5 +1,22 @@
+import java.io.*;
+
 public class App {
+
+    public static void contenido(String archivo) throws FileNotFoundException, IOException{
+        String cadena;
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null){
+            String[] bar = cadena.split(" ");
+            for(int i = 0; i < bar.length; i++){
+                if(bar[i] == "eiffel"){
+                    System.out.println(bar[i]);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        contenido("/mnt/c/Users/Diego/Desktop/Tarea-3-SO/Tarea 3/src/palabras.txt");
     }
 }
