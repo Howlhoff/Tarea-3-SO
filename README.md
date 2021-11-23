18e092d554729d7c9190486b411eb17f53a7405b
Howlhoff <diego.beltran@sansano.usm.cl>
2021-11-23 20:27:52 -0300

    Tarea 3 v1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tarea-3-SO
+
+Comandos de instruccion:
+1) javac buscador.java (compilacion)
+2) java buscador.java (ejecucion)
--- src/tarea3/buscador.java
+++ src/tarea3/buscador.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class buscador {
 	public static void main(String[] args) throws Exception {
-        String archivo = "C:\\Users\\Diego\\Desktop\\Tarea-3-SO\\src\\tarea3\\palabras.txt";
+        String archivo = "palabras.txt";
         String cadena;
         FileReader f = new FileReader(archivo);
 		BufferedReader b = new BufferedReader(f);
