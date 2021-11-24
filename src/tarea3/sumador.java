23f12f8061218f187e7beb6e57fd4b2fd5cccb22
UsrBern <bernardo.pinninghoff@usm.cl>
2021-11-24 16:51:21 -0500

    Hash fix

--- src/tarea3/sumador.java
+++ src/tarea3/sumador.java
@@ -6,30 +6,33 @@ public class sumador implements Runnable{
 
     @Override
     public void run(){
-        FileReader fp = new FileReader("numeros.txt");
-        BufferedReader buff = new BufferedReader(fp);
-        Boolean flag = false;
-        String line;
-        int a, b;
+        try {
+            FileReader fp = new FileReader("numeros.txt");   
+            BufferedReader buff = new BufferedReader(fp);
+            Boolean flag = false;
+            String line;
+            int a, b;
 
-        while ((line = buff.readLine()) != null && !flag){
-            String[] nums = line.split(" ");
-            for (int i = 0; i < nums.length-1; i++){
-                a = Integer.parseInt(nums[i]);
-                for (int j = i+1; j < nums.length; j++){
-                    b = Integer.parseInt(nums[j]);
-                    if (a+b == 65){
-                        System.out.println("Sumador: "+nums[i]+" + "+nums[j]+" = 65");
-                        flag = true;
-                        break;
+            while ((line = buff.readLine()) != null && !flag){
+                String[] nums = line.split(" ");
+                for (int i = 0; i < nums.length-1; i++){
+                    a = Integer.parseInt(nums[i]);
+                    for (int j = i+1; j < nums.length; j++){
+                        b = Integer.parseInt(nums[j]);
+                        if (a+b == 65){
+                            System.out.println("Sumador: "+nums[i]+" + "+nums[j]+" = 65");
+                            flag = true;
+                            break;
+                        }
+
                     }
-
                 }
             }
+
+            buff.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
-        buff.close();
     }
     
 }
