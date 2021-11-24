f390fb0c747db62dbbd7049109edd6300621f220
UsrBern <bernardo.pinninghoff@usm.cl>
2021-11-24 18:45:28 -0500

    minor fix

--- src/tarea3/Main.java
+++ src/tarea3/Main.java
@@ -3,21 +3,21 @@ package tarea3;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException{
-		Buscador tarea1 = new Buscador();
-		Sumador tarea2 = new Sumador();
-		Decodificador tarea3 = new Decodificador();
+		tarea3.Buscador task1 = new Buscador();
+		tarea3.Sumador task2 = new Sumador();
+		tarea3.Decodificador task3 = new Decodificador();
 		
-		tarea1.start();
-		tarea2.start();
-		tarea3.start();
+		task1.start();
+		task2.start();
+		task3.start();
 
-		while(tarea1.isAlive() == true || tarea2.isAlive() == true || tarea3.isAlive() == true){
+		while(task1.isAlive() == true || task2.isAlive() == true || task3.isAlive() == true){
 			//Aqui no pasa nada, solo se espera a que terminen los threads
 		}
 
-		System.out.println("Tiempo de Ejecucion de Buscador: " + tarea1.getRuntime());
-		System.out.println("Tiempo de Ejecucion de Sumador: " + tarea2.getRuntime());
-		System.out.println("Tiempo de Ejecucion de Decodificador: " + tarea3.getRuntime());
+		System.out.println("Tiempo de Ejecucion de Buscador: " + task1.getRuntime());
+		System.out.println("Tiempo de Ejecucion de Sumador: " + task2.getRuntime());
+		System.out.println("Tiempo de Ejecucion de Decodificador: " + task3.getRuntime());
 	}
 
 }
